82d16862683c458fcd86d2f9db21d7dd399acfba
kangsunwoo827 <sunwoo827@snu.ac.kr>
2023-06-21 16:22:18 +0900

    git

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
-import requests
+from crawler import Crawler
+import json
+import telegram
+import asyncio
 
-url = "https://api.upbit.com/v1/ticker?markets=KRW-BTC"
+krw_volume=Crawler().getVolumes()
+volume_text = format(krw_volume,',')
+send_text = f'24 hour accumulated trade value of Upbit is {volume_text} KRW'
 
-headers = {"accept": "application/json"}
 
-response = requests.get(url, headers=headers)
 
-print(response.text)
+api_key = json.load(open('secret.json'))['BotKey']
+
+bot = telegram.Bot(token = api_key)
+
+chat_id = json.load(open('secret.json'))['ChatID']
+
+
+asyncio.run(bot.send_message(chat_id = chat_id, text=send_text))
